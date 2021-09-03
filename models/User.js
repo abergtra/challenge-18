0be7807656b1d5ba82cf9c08792e6dda0f294670
Asher <asher.bergtraun@gmail.com>
2021-09-02 22:09:06 -0700

    completed user model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+//require mongoose
+const { Schema, model } = require('mongoose');
+
+//guidelings to set up models and API routes
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'What is your username?',
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: 'What is your email address?',
+            validate: {
+                validator(validEmail) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(validEmail);
+                },
+                message: 'That email was not valid, please try again.'
+            }
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'ThouUserght'
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+//Schema settings
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length
+})
+
+//export model
+const User = model('User', UserSchema)
+module.exports = User
