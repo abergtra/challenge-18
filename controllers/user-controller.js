9e17edebf4cb49ba4f7560846a7c573e0adb0c3e
Asher <asher.bergtraun@gmail.com>
2021-09-09 01:20:30 +0100

    built user controller functions framework

---
+++ controllers/user-controller.js
@@ -0,0 +1,27 @@
+//Import User and Thought models
+const { User, thought } = require('../models');
+
+//User functions
+const userController = {
+    getAllUsers(req, res) {
+
+    },
+    getUserByID({ params }, res) {
+
+    },
+    createNewUser({ body }, res) {
+
+    },
+    updateUser({ params, body }, res) {
+
+    },
+    deleteUser({ params }, res) {
+
+    },
+    addFriend({ params }, res) {
+
+    },
+    deleteFriend({ params }, res){
+        
+    }
+}
