0d5c2ff59a9f0c1cb413d3bb31dfe32c91c1963f
Asher <asher.bergtraun@gmail.com>
2021-09-27 08:46:18 -0700

    added api endpoints to thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,6 +1,7 @@
 //require express for routing api
 const router = require('express').Router();
 
+const { route } = require('.');
 //connect to functions in controller
 const{
     getAllThoughts,
@@ -10,3 +11,25 @@ const{
     addReaction,
     deleteReaction
 } = require('../../controllers/thought-controller')
+
+//set up api endpoints for /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+
+router
+    .route('/:userId')
+    .post(createNewThought)
+
+router
+    .route('/:thoughtId')
+    .get(getThoughtByID)
+    .put(createNewThought)
+    .delete(deleteThought)
+
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction)
+    .delete(deleteReaction)
+
+module.exports = router;
