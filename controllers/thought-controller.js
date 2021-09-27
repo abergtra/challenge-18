701027f58cd0695a883a02e2f081d73fe3e32471
Asher <asher.bergtraun@gmail.com>
2021-09-27 06:47:41 -0700

    created deletethought controller framework

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -51,7 +51,19 @@ const thoughtController = {
     },
     //delete an existing thought
     deleteThought({ params }, res) {
-
+        console.log("retrieved params", params)
+        Thought.findOne({ _id: params.thoughtID})
+        .select("-__v")
+        .then((dbThoughtData) => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: "Cannot find thought with this id."});
+                return;
+            } res.json(dbThoughtData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
     },
     //add a new reaction
     addReaction({ params, body }, res) {
