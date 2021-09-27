cf794ce9c90685b515838ec33f6731268015e4c4
Asher <asher.bergtraun@gmail.com>
2021-09-27 06:31:58 -0700

    built out getallthoughts controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -4,7 +4,14 @@ const { Thought, User, Types } = require('../models');
 //Thought functions
 const thoughtController = {
     getAllThoughts(req, res) {
-
+        Thought.find({})
+        .select("-__v")
+        .sort({ _id: -1 })
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
     },
     getThoughtByID({ params }, res) {
 
