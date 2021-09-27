60516a4199ebdfea395631b5ba70358aa6df4c6c
Asher <asher.bergtraun@gmail.com>
2021-09-27 06:20:39 -0700

    required routes folder and connected to mongoos

--- server.js
+++ server.js
@@ -9,3 +9,19 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+//require routes
+app.use(require('./routes'));
+
+//connect to mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/challenge-18',{
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//set mongoose to log actions for debugging
+mongoose.set('debug', true);
+
+//connect to PORT
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
