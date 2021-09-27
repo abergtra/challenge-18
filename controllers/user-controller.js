4885f48b31fec2fcc8137c4edef1590dd98d0868
Asher <asher.bergtraun@gmail.com>
2021-09-27 07:34:22 -0700

    used thought controller to make user controller framework

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,25 +4,102 @@ const { User, Thought } = require('../models');
 //User functions
 const userController = {
     getAllUsers(req, res) {
-
+        Thought.find({})
+        .select("-__v")
+        .sort({ _id: -1 })
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
     },
     getUserByID({ params }, res) {
-
+        console.log("retrieved params", params)
+        Thought.findOne({ _id: params.thoughtID})
+        .select("-__v")
+        .then((dbThoughtData) => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: "Cannot find thought with this id."});
+                return;
+            } res.json(dbThoughtData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
     },
     createNewUser({ body }, res) {
-
+        console.log("INCOMING BODY", body)
+        Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: params.userId },
+                { $push: { thoughts: _id } },
+                { new: true }
+            );
+        })
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "Cannot find user with this id."});
+                return;
+            } res.json(dbUserData);
+        })
+        .catch((err) => res.json(err));
     },
     updateUser({ params, body }, res) {
-
+        Thought.findOne({ _id: params.thoughtID})
+        .select("-__v")
+        .then((dbThoughtData) => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: "Cannot find thought with this id."});
+                return;
+            } res.json(dbThoughtData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
     },
     deleteUser({ params }, res) {
-
+        Thought.findOneAndDelete({ _id: params.thoughtID})
+        .then(deletedthought => {
+            if (!deletedthought) {
+                return res.status(404).json({ message: "Cannot find thought with this id."});
+            } return User.findOneAndUpdate(
+                { _id: params.username },
+                { $pull: { thoughts: params.thoughtId } },
+                { new: true }
+            );
+        })
+        .then(dbUserData => {
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
     },
     addFriend({ params }, res) {
-
+        console.log("INCOMING BODY", body)
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body } },
+            { new: true }
+        )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'Cannot find user with this id.' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
     },
     deleteFriend({ params }, res){
-
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: { reactionId: params.reactionId } } },
+            { new: true }
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
     }
 }
 
