2ffdd553d76335940139c8169f72cb363c23420e
Asher <asher.bergtraun@gmail.com>
2021-09-27 06:54:49 -0700

    added framework for addreaction controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -68,7 +68,23 @@ const thoughtController = {
     },
     //add a new reaction
     addReaction({ params, body }, res) {
-
+        console.log("INCOMING BODY", body)
+        Thought.create(body)
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $push: { thoughts: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id! first error' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
     },
     //delete and existing reactioin
     deleteReaction({ params }, res){
